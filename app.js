935d1bb93042755b85ba3c80cc22e1a1d455fd1a
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-06-12 01:16:12 -0400

    add answer validation

--- app.js
+++ app.js
@@ -22,12 +22,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub username'
+            message: 'Enter your GitHub username (Required)',
+            validate: gitUserInput => {
+                if (gitUserInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -57,7 +73,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -68,7 +92,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: gitLinkInput => {
+                if (gitLinkInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the GitHub link to your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
