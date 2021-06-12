f8e6e279df5fa139249fe042b2c4623a652b145c
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-06-12 00:18:45 -0400

    install inquirer

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-// In order to use the fs (File Structure) module, need to include the following at the top of JS
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs); // Prints ['Leslie', 'Web Developer'] if enter "node app 'Leslie' 'Web Developer'" into terminal
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
 // const printProfileData = profileDataArr => {
 //     // This code produces the same results as the code below
@@ -17,8 +13,27 @@ const [name, github] = profileDataArgs;
 
 // printProfileData(profileDataArgs); // Prints Leslie and Web Developer on separate lines
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// CODE START
+// In order to use the fs (File Structure) module, need to include the following at the top of JS
+const inquirer = require('inquirer');
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+    
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
